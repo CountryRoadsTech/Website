64f37debcceaaaec1ac0ef62f552f32ce4a9a9ba
Addison Martin <addisonejmartin@gmail.com>
2020-10-01 14:07:28 -0400

    Added protection from cross-site request forgery attacks.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  # Enables protection from cross-site request forgery (CSRF) attacks.
+  protect_from_forgery with: :exception
 
   before_action :set_sentry_raven_context
 
