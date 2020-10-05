0aa41cb1831e7bb3608a3e603d8cdb410bbda8e2
Addison Martin <addisonejmartin@gmail.com>
2020-10-05 19:48:50 -0400

    Created user authentication controllers, inheriting from devise's controllers. Removed omniauth from user authentication as it is not used.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  devise_for :users
+  # Adds user authentication routes.
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
 end
