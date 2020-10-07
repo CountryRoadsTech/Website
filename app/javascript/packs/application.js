bcc6400ebdb923b64b6883af5236f1960351c56f
Addison Martin <addisonejmartin@gmail.com>
2020-10-06 21:49:29 -0400

    Installed and configured active storage for file attachments.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -23,3 +22,6 @@ import '../stylesheets/application.scss'
 
 // Import user analytic's Javascript.
 import ahoy from 'ahoy.js';
+
+// Display upload status when direct uploading files.
+import '../source/direct_upload'
