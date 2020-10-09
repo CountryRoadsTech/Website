f9066fdca8bd6215bba816986fe2fc8c85409519
Addison Martin <addisonejmartin@gmail.com>
2020-10-08 21:14:45 -0400

    Converted main application.html file to .slim

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,5 +9,10 @@ import controller from './application_controller'
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
+const context_components = require.context("../../components", true, /_controller.js$/)
+application.load(
+    definitionsFromContext(context).concat(
+        definitionsFromContext(context_components)
+    )
+)
 StimulusReflex.initialize(application, { consumer, controller, debug: false })
