812e2c28ad86f6ac4da84c84ba978afa6f1f54d2
Addison Martin <addisonmartin@hey.com>
2021-03-05 17:22:10 -0500

    Added Bootstrap icons.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@
 
 # Helper methods here are available across the application (where model-specific helpers are available).
 module ApplicationHelper
+  # Embeds Bootstrap SVG icons into HTML.
+  def icon(icon, options = {})
+    file = File.read("node_modules/bootstrap-icons/icons/#{icon}.svg")
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css('svg')
+
+    # Apply any of Bootstrap's classes, if provided.
+    svg['class'] += ' ' + options[:class] if options[:class].present?
+
+    doc.to_html.html_safe
+  end
 end
